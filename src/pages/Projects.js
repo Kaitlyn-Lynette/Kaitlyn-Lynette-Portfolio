adffbecdda0bd8e2b2b8ddb1df7c183bc099cd61
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2020-07-16 14:20:20 -0400

    Added the projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,64 +1,67 @@
 import React from "react";
+const project1 = "https://kaitlyn-lynette.github.io/COVID-19-Travel-Information-Project-1/"
+const project2 = "https://peliculy.herokuapp.com/"
+const project3 =  "https://chirrup-app.herokuapp.com/"
 
 
 function Projects () {
     return (
-    <section id="work">
-    <div className="container">
-        <div className="row">
-
-            <div className="col-md-5 col-sm-10">
+        <section id="work">
+        <div className="container">
+             <div className="row">
+   
+                  <div className="col-md-5 col-sm-10">
+                       
+                       <div className="section-title">
+                            <h5>Featured Projects</h5>
+                            <h4>I design creative stuffs & build online digital products related business passionate.</h4>
+                       </div>
+                  </div>
+   
+                  <div className="clearfix"></div>
+   
+                  <div className="col-md-4 col-sm-6">
+                   
+                       <div className="work-thumb">
+                            <a href="images/work-1.jpg" className="image-popup">
+                                 <img src="images/work-1.jpg" className="img-responsive" alt="Work" />
+                            </a>
+                       </div>
+                       <div className="work-overlay">
+                            {/* <h5>Covid Travel Planner</h5> */}
+                            <h4><a href="https://kaitlyn-lynette.github.io/COVID-19-Travel-Information-Project-1/">Covid Travel Planner</a></h4>
+                       </div>
+                  </div>
+   
+                  <div className="col-md-4 col-sm-6">
                     
-                    <div className="section-title">
-                        <h5>Featured Projects</h5>
-                        <h4>I haved designed and built the following projects during the course of the bootcamp.</h4>
-                    </div>
-            </div>
-
-            <div className="clearfix"></div>
-
-            <div className="col-md-4 col-sm-6">
-                    
-                    <div className="work-thumb">
-                        <a href="images/work-1.jpg" className="image-popup">
-                            <img src="images/work-1.jpg" className="img-responsive" alt="Work" />
-                        </a>
-                    </div>
-                    <div className="work-overlay">
-                        <h5>Covid Travel Planner</h5>
-                        <h4>Wood D sTudio</h4>
-                    </div>
-            </div>
-
-            <div className="col-md-4 col-sm-6">
-                    
-                    <div className="work-thumb">
-                        <a href="images/work-1.jpg" className="image-popup">
-                            <img src="images/work-1.jpg" className="img-responsive" alt="Work" />
-                        </a>
-                    </div>
-                    <div className="work-overlay">
-                        <h5>Peliculy</h5>
-                        <h4>Maladon Zone Lite</h4>
-                    </div>
-            </div> 
-
-            <div className="col-md-4 col-sm-6">
-                
-                    <div className="work-thumb">
-                        <a href="images/work-1.jpg" className="image-popup">
-                            <img src="images/work-1.jpg" className="img-responsive" alt="Work" />
-                        </a>
-                    </div>
-                    <div className="work-overlay">
-                        <h5>Chirrup!</h5>
-                        <h4>Drink Palon</h4>
-                    </div>
-            </div>      
-
+                       <div className="work-thumb">
+                            <a href="images/work-2.jpg" className="image-popup">
+                                 <img src="images/work-2.jpg" className="img-responsive" alt="Work" />
+                            </a>
+                       </div>
+                       <div className="work-overlay">
+                            {/* <h5>Peliculy</h5> */}
+                            <h4><a href="https://peliculy.herokuapp.com/">Peliculy</a></h4>
+                       </div>
+                  </div> 
+   
+                  <div className="col-md-4 col-sm-6">
+                 
+                       <div className="work-thumb">
+                            <a href="images/work-3.jpg" className="image-popup">
+                                 <img src="images/work-3.jpg" className="img-responsive" alt="Work" />
+                            </a>
+                       </div>
+                       <div className="work-overlay">
+                            {/* <h5>Chirrup</h5> */}
+                            <h4><a href="https://chirrup-app.herokuapp.com/">Chirrup!</a></h4>
+                       </div>
+                  </div>      
+   
+             </div>
         </div>
-    </div>
-    </section>
+        </section>
     )
 }
 
--- src/pages/Testimonial.js
+++ src/pages/Testimonial.js
@@ -19,7 +19,7 @@ function Testimonial () {
                <div className="clearfix"></div>
 
                <div className="col-md-offset-2 col-md-8 col-sm-12">
-                    <div id="owl-testimonial" className="owl-carousel">
+                    {/* <div id="owl-testimonial" className="owl-carousel"> */}
                          <div className="item">
                               <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
                                    <div className="tst-author">
@@ -59,7 +59,7 @@ function Testimonial () {
                                    </div>
                          </div>
 
-                    </div>
+                    {/* </div> */}
                </div>
 
           </div>
