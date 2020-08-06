6b611bba47893a8f08db29a739a0b748a886df33
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2020-08-06 14:31:02 -0400

    Added another column to work for presentation

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,36 +4,31 @@ import React from "react";
 function Projects () {
     return (
         <section id="work">
-        <div className="container">
-             <div className="row">
-   
-                  <div className="clearfix"></div>
+          <div className="container">
+               <div className="row">
+                    <div className="clearfix"></div>
 
-                  <div className="col-md-4 col-sm-6">
-
-                       <div className="work-title">
-                            <h2>Latest Project</h2>
-                       </div>
-                 
-                       <div className="work-thumb">
-                            {/* <a href="images/work-3.jpg" className="image-popup">
-                                 <img src="images/work-3.jpg" className="img-responsive" alt="Work" />
-                            </a> */}
-                            <video width="300" height="500" controls>
-                            <source src="images/movie.mp4" type="video/mp4" />
-                            </video>
-                       </div>
-                       <div className="title">
-                            {/* <h5><a href="https://chirrup-app.herokuapp.com/">Check it out!</a></h5> */}
-                            <h4>Chirrup!</h4>
-                       </div>
-                       <div className="summary">
-                       <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRO2m3wfUIq8nsxCfB-gTaGLVSiW2AvHHuuyWVZ9PyS3XyJ9SjgEc6uPwQ4SyLElEhNpUKxo-CnOy1r/embed?start=true&loop=true&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
-                       </div>
-                  </div>      
-   
-             </div>
-        </div>
+                    <div className="col-md-4 col-sm-6">
+                         <div className="work-title">
+                              <h2>Latest Project</h2>
+                         </div>
+                    
+                         <div className="work-thumb">
+                              <video width="300" height="500" controls>
+                              <source src="images/movie.mp4" type="video/mp4" />
+                              </video>
+                         </div>
+                         <div className="title">
+                              <h4>Chirrup!</h4>
+                         </div>
+                    </div>
+                    <div className="col-md-3 col-sm-12">
+                         <div className="work-presentation">
+                              <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRO2m3wfUIq8nsxCfB-gTaGLVSiW2AvHHuuyWVZ9PyS3XyJ9SjgEc6uPwQ4SyLElEhNpUKxo-CnOy1r/embed?start=true&loop=true&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+                         </div>
+                    </div>
+               </div>
+           </div>
         </section>
     )
 }
