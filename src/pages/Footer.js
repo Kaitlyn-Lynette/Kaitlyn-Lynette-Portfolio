17e672a68f242169666206c1d31a6146157f076f
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2020-08-07 22:18:03 -0400

    Removed social media links

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -11,14 +11,6 @@ function Footer () {
                          <p> tel - <a href="tel:6462431361">+(646) 243 1361</a> | email - <a href="kaitlyn.l.rodriguez@gmail.com">Site code implemented by Kaitlyn</a></p>
                     </div>
                </div>
-
-               {/* <div class="col-md-4 col-sm-5">
-                    <ul class="social-icon">
-                         <li><a href="https://www.linkedin.com/in/krodriguez93/" class="fa fa-linkedin"></a></li>
-                         <li><a href="https://www.facebook.com/kaitlyn.rodriguez.52" class="fa fa-facebook"></a></li>
-                         <li><a href="https://github.com/Kaitlyn-Lynette" class="fa fa-github"></a></li>
-                    </ul>
-               </div> */}
                
           </div>
         </div>
