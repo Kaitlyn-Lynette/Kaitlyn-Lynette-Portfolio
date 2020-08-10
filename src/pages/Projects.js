0cea72b64b4d6c73fea088b41d300b44c25dfc42
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2020-08-10 15:36:41 -0400

    Added media query for iphone X to css

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -22,9 +22,10 @@ function Projects () {
                          <h4>Chirrup!</h4>
                     </div>
                </div>
-               <div className="col-md-3 col-sm-12">
+               <div className="col-md-4 col-sm-6">
+                         <h2>Pitch Deck</h2>
                     <div className="work-presentation">
-                         <iframe title= "presentation" src="https://docs.google.com/presentation/d/e/2PACX-1vRO2m3wfUIq8nsxCfB-gTaGLVSiW2AvHHuuyWVZ9PyS3XyJ9SjgEc6uPwQ4SyLElEhNpUKxo-CnOy1r/embed?start=true&loop=true&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+                         <iframe title= "presentation" src="https://docs.google.com/presentation/d/e/2PACX-1vRO2m3wfUIq8nsxCfB-gTaGLVSiW2AvHHuuyWVZ9PyS3XyJ9SjgEc6uPwQ4SyLElEhNpUKxo-CnOy1r/embed?start=true&loop=true&delayms=3000" frameborder="0" width="300" height="500" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
                     </div>
                </div>
           </div>
