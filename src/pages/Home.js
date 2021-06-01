4b1521be02fb7c30e8699a83f79242310f83a9ce
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-06-01 17:25:56 -0400

    Made updates to the Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ function Home () {
                <div className="col-md-6 col-sm-6">
                     <div className="home-text">
                          <h5>hey there, this is my personal site.</h5>
-                         <h1>I am a new web developer based in South Brooklyn.</h1>
+                         <h1>I started developing websites to promote my own small business ideas and those of my friends. Check out some of my work. </h1>
                          <a href="/about" className="btn section-btn smoothScroll">Learn More<i className="fa fa-angle-right"></i></a>
                     </div>
                </div>
