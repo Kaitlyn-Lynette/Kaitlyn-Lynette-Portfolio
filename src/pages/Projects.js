aa23ccbb77a8e1cb8959a5ec60fba1e6d604a336
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-06-01 17:39:36 -0400

    Added additional projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,9 +8,17 @@ function Projects () {
                <div className="row">
                     <div className="clearfix"></div>
 
+
                     <div className="col-md-4 col-sm-6">
                          <div className="work-title">
-                              <h2>Latest Project</h2>
+                              <h2>Lifstyle Brand Project on Shopify</h2>
+                              <a href="https://calmateco.com/"> <h4>Calmate</h4></a>
+                         </div>
+                              
+                    </div>
+                    <div className="col-md-4 col-sm-6">
+                         <div className="work-title">
+                              <h2>React Mobile Web Application</h2>
                          </div>
                     
                          <div className="work-thumb">
@@ -23,7 +31,13 @@ function Projects () {
                               
                          </div>
                     </div>
-                    <div className="col-lg-8 col-md-6 col-sm-6">
+                    <div className="col-md-4 col-sm-6">
+                         <div className="work-title">
+                              <h2>NonProfit Site on Wordpress</h2>
+                              <a href="http://karama.nyc/"> <h4>Karama Kids</h4></a>
+                         </div>     
+                    </div>
+                    {/* <div className="col-lg-8 col-md-6 col-sm-6">
                          <div className="presentation-title">
                               <h2>Pitch Deck</h2>
                          </div>
@@ -31,7 +45,7 @@ function Projects () {
                          <div className="work-presentation">
                               <iframe title= "presentation" src="https://docs.google.com/presentation/d/e/2PACX-1vRO2m3wfUIq8nsxCfB-gTaGLVSiW2AvHHuuyWVZ9PyS3XyJ9SjgEc6uPwQ4SyLElEhNpUKxo-CnOy1r/embed?start=true&loop=true&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
                          </div>
-                    </div>
+                    </div> */}
                </div>
           </div>
      </section>
