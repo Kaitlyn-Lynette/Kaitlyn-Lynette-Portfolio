f9ffaf0b2f3c684888261531e32e366bf35672f6
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-06-05 23:12:38 -0400

    Added elastic carousel

---
+++ src/components/carousel/Item.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background-color: #FFFFFF;
+  color: #fff;
+  margin: 0 15px;
+  font-size: 4em;
+`;
--- src/pages/Testimonial.js
+++ src/pages/Testimonial.js
@@ -1,10 +1,20 @@
 import React from "react";
-import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
+import Carousel from "react-elastic-carousel";
+import Item from "../components/carousel/Item";
+import "../components/carousel/styles.css";
+
+// import OwlCarousel from 'react-owl-carousel';
+// import 'owl.carousel/dist/assets/owl.carousel.css';
+// import 'owl.carousel/dist/assets/owl.theme.default.css';
+
+const breakPoints = [
+     // { width: 1, itemsToShow: 1 },
+     // { width: 550, itemsToShow: 2 },
+     // { width: 768, itemsToShow: 3 },
+     { width: 1200, itemsToShow: 1 },
+   ];
 
 function Testimonial () {
-
     return (
 
 <section id="testimonial">
@@ -21,9 +31,10 @@ function Testimonial () {
                <div className="clearfix"></div>
 
                <div className="col-md-offset-2 col-md-8 col-sm-12">
-               <OwlCarousel className='owl-theme'>
+               {/* <OwlCarousel className='owl-theme'> */}
                     {/* <div id="owl-testimonial" className="owl-carousel"> */}
-                         <div className="item">
+                         <Carousel breakPoints={breakPoints}>
+                         <Item>
                               <p>"I had the pleasure of managing Kaitlyn in her Guest Engagement Technical Specialist role at Olo. Kaitlyn constantly sought opportunities to challenge herself and grow her technical abilities, going above and beyond role expectations, particularly in her leading of OAuth and CLASS certifications. Kaitlyn's tenacity and genuine interest in the technical side of implementations gave our team the confidence to dive more deeply into troubleshooting and problem solving, enabling us to better support our customers and partners."</p>
                                    <div className="tst-author">
                                         <div className="tst-foto">
@@ -34,10 +45,9 @@ function Testimonial () {
                                              <h5>Director, Customer Success at Olo</h5>
                                         </div>
                                    </div>
-                         </div>
-                   
+                         </Item>
 
-                         <div className="item">
+                         <Item>
                               <p>"Kaitlyn's dedication is unmatched! I had the pleasure of being paired up with her to aid in the transition with our offshore support Kaitlyn quickly identified the need for in-person training instead of our initial remote approach, which resulted in her 8-week stay in Bogota, Colombia. During her time there she not only ensured the new team was up to speed but never lost sight of her responsibilities as a technical specialist. Her perseverance, strategic thinking, and attention to detail combined with her infectious energy truly set her apart. I would welcome any opportunity to work with Kaitlyn again.  "</p>
                                    <div className="tst-author">
                                         <div className="tst-foto">
@@ -48,9 +58,9 @@ function Testimonial () {
                                              <h5>Sr. Manager, Customer Success at Olo</h5>
                                         </div>
                                    </div>
-                         </div>
+                         </Item>
 
-                         <div className="item">
+                         <Item>
                               <p>"Kaitlyn was instrumental in leading my onboarding onto the Guest Experience team. Amongst the numerous tasks, issues, and projects that she had on her plate, she always dedicated time to share her product knowledge and strategies with me, which helped me develop in my role immensely. She never backed away from challenging integrations or troubleshooting issues and consistently exemplified what going above and beyond for internal and external teams meant. Her straightforward thinking and problem solving skills pushed our team to continually identify ways to improve our processes and come up with custom solutions for clients. Kaitlyn's work discipline and dedication would make her a great asset to any team!"</p>
                                    <div className="tst-author">
                                         <div className="tst-foto">
@@ -61,9 +71,9 @@ function Testimonial () {
                                              <h5>Techinical Specialist, Customer Success at Olo</h5>
                                         </div>
                                    </div>
-                         </div>
+                         </Item>
 
-                         <div className="item">
+                         <Item>
                               <p>"Kaitlyn is without a doubt one of most hardworking and talented students I've had the privilege to teach. From the very beginning of my time teaching her she demonstrated a formidable tenacity in her approach to problem solving that was rivaled only by her focus. Kaitlyn consistently tested herself working on either end of the stack, repeatedly gravitating away from her established strengths to sharpen her skills in areas she believed she could still improve, seeing each challenge as an opportunity for deeper understanding. An exercise was not over when Kaitlyn got her code to run but only when she completely understood how every piece fit together. She showed an early aptitude for both MongoDB and SQL but has demonstrated a consistent passion for React.js, continuing to study and build React apps with her former classmates long after the course concluded. As a teammate Kaitlyn has a unique ability to exude authority keeping her team organized and on task while still maintaining an air of camaraderie with her natural charisma. I expect much success in her future as an engineer and can say she would be a valuable addition to any development team without question."</p>
                                    <div className="tst-author">
                                         <div className="tst-foto">
@@ -74,11 +84,10 @@ function Testimonial () {
                                              <h5>Course Assistant Columbia University, Software Engineer Aquent</h5>
                                         </div>
                                    </div>
-                         </div>
-                    {/* </div> */}
-               </OwlCarousel>
-               </div>
-                    
+                         </Item>
+                         </Carousel>
+              </div>
+               {/* </OwlCarousel> */}
           </div>
      </div>
 </section>
