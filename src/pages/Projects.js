a6393a1db252fa9d546b2063eed70fa1d638ff74
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-06-07 09:07:09 -0400

    Karama Kids

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -27,6 +27,7 @@ function Projects () {
                     </div>
                   
                     <div className="col-md-4 col-sm-6">
+
                          <div className="work-title">
                               <h2>React Mobile Web Application</h2>
                               <div className="title">
@@ -40,21 +41,19 @@ function Projects () {
                               </video>
                          </div>
                     </div>
+                    
                     <div className="col-md-4 col-sm-6">
                          <div className="work-title">
                               <h2>NonProfit Site on Wordpress</h2>
                               <a href="http://karama.nyc/"> <h4>Karama Kids</h4></a>
                          </div>     
+                   
+                         <div className="work-thumb">
+                              <video width="300" height="500" controls>
+                              <source src="images/karamakids.mp4" type="video/mp4" />
+                              </video>
+                         </div>
                     </div>
-                    {/* <div className="col-lg-8 col-md-6 col-sm-6">
-                         <div className="presentation-title">
-                              <h2>Pitch Deck</h2>
-                         </div>
-               
-                         <div className="work-presentation">
-                              <iframe title= "presentation" src="https://docs.google.com/presentation/d/e/2PACX-1vRO2m3wfUIq8nsxCfB-gTaGLVSiW2AvHHuuyWVZ9PyS3XyJ9SjgEc6uPwQ4SyLElEhNpUKxo-CnOy1r/embed?start=true&loop=true&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
-                         </div>
-                    </div> */}
                </div>
           </div>
      </section>
